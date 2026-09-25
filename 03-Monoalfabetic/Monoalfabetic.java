8a6843e1a3ce801049b074074b061aae618d10f9
nico <2024_nicolas.lison@iticbcn.cat>
2026-09-25 19:48:51 +0200

    Excercici 3 terminat

--- 03-Monoalfabetic/Monoalfabetic.java
+++ 03-Monoalfabetic/Monoalfabetic.java
@@ -6,16 +6,6 @@ public class Monoalfabetic {
 
     public static char[] majuscules = "AÁÀBCÇDEÉÈFGHIÍÌÏJKLMNÑOÓÒPQRSTUÚÙÜVWXYZ".toCharArray();
     public static char[] permutat = permutaAlfabet(majuscules);
-    
-
-    private static int indexOf(char[] array, char c) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == c) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
     public static char[] permutaAlfabet(char[] alfabet) {
         List<Character> llista = new ArrayList<>();
@@ -54,8 +44,64 @@ public class Monoalfabetic {
  
         return new String(sortida);
     }
-
+ 
+    public static String desxifraMonoAlfa(String cadena) {
+        char[] xifrada = cadena.toCharArray();
+        char[] sortida = new char[xifrada.length];
+ 
+        for (int i = 0; i < xifrada.length; i++) {
+            char c = xifrada[i];
+            char majuscula = Character.toUpperCase(c);
+            int index = indexOf(permutat, majuscula);
+ 
+            if (index == -1) {
+                sortida[i] = c;
+            } 
+            else {
+                char original = majuscules[index];
+                if (Character.isLowerCase(c)) {
+                    original = Character.toLowerCase(original);
+                }
+                sortida[i] = original;
+            }
+        }
+ 
+        return new String(sortida);
     }
 
-}
  
+    private static int indexOf(char[] array, char c) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == c) {
+                return i;
+            }
+        }
+        return -1;
+    }
+ 
+    public static void main(String[] args) {
+        System.out.println("Alfabet original: " + new String(majuscules));
+        System.out.println("Alfabet permutat: " + new String(permutat));
+        System.out.println();
+ 
+        String[] tests = {
+                "àrbitre, coixi, Perímetre",
+                "Taüll, DÍA, año",
+                "Peça, Òrrius, Bòvila"
+        };
+ 
+        System.out.println("Xifratge:");
+        String[] xifrats = new String[tests.length];
+        for (int i = 0; i < tests.length; i++) {
+            xifrats[i] = xifraMonoAlfa(tests[i]);
+            System.out.println("Test " + i + " " + tests[i]+ " -> " + xifrats[i]);
+        }
+ 
+        System.out.println();
+        System.out.println("Desxifratge:");
+        for (int i = 0; i < xifrats.length; i++) {
+            String desxifrat = desxifraMonoAlfa(xifrats[i]);
+            System.out.println("Test " + i + " " + xifrats[i] + " -> " + desxifrat);
+        }
+    }
+}
